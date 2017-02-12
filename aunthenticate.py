60b6c3b7012d3128a6b2769af713a0c38849455e
Roshan Nair <rsnair@andrew.cmu.edu>
2017-02-11 22:25:22 -0500

    Final commit...with no team mates = failure

--- aunthenticate.py
+++ aunthenticate.py
@@ -1,3 +1,3 @@
 import plotly 
-plotly.tools.set_credentials_file(username='rnair18', api_key='4S0yHkL8pyJfqK2GbN4O')
+plotly.tools.set_credentials_file(username='temp18', api_key='todGRKLb6SOXJMk9X8L1')
 
